bc076749a9688af86b4585fbad8dbc3df1d7aa65
Herman <57533713+hratas@users.noreply.github.com>
2023-10-20 12:57:02 +0300

    Update streamlit_app.py
    Fixing typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Fruit pick list
-streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index),['Avocado','Strawberry'])
+streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index),['Avocado','Strawberries'])
 
 # Display fruit list
 streamlit.dataframe(my_fruit_list)
