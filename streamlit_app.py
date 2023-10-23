92b20e011595a1714810dcd813ad99ef470c835c
Herman <57533713+hratas@users.noreply.github.com>
2023-10-23 16:05:54 +0300

    Update streamlit_app.py
    Testing Snowflake DB insertion

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,6 @@ streamlit.dataframe(my_data_row)
 # Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', '')
 streamlit.write('Thanks for adding', add_my_fruit)
+
+# Something wrong with this command according to course
+my_cur.execute('insert into fruit_load_list values("from streamlit"))
